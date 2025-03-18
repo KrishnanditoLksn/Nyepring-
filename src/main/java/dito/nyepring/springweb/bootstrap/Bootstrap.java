365c0b8023036b0350857c874d445e683bfe9473
KrishnanditoLksn <krislaksana123@gmail.com>
2025-03-18 19:37:50 +0700

    feat:init all data and count rows in db

--- src/main/java/dito/nyepring/springweb/bootstrap/Bootstrap.java
+++ src/main/java/dito/nyepring/springweb/bootstrap/Bootstrap.java
@@ -2,8 +2,10 @@ package dito.nyepring.springweb.bootstrap;
 
 import dito.nyepring.springweb.domain.Author;
 import dito.nyepring.springweb.domain.Book;
+import dito.nyepring.springweb.domain.Publisher;
 import dito.nyepring.springweb.repositories.AuthorRepository;
 import dito.nyepring.springweb.repositories.BookRepository;
+import dito.nyepring.springweb.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,11 +13,12 @@ import org.springframework.stereotype.Component;
 public class Bootstrap implements CommandLineRunner {
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-
-    public Bootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public Bootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -37,21 +40,39 @@ public class Bootstrap implements CommandLineRunner {
         messi.setFirstName("Lionel");
         messi.setLastName("Messi");
 
+
+        Publisher publisher = new Publisher();
+        publisher.setAddress("Jalan Kaliurang");
+        publisher.setCity("Sragen");
+        publisher.setPublisherName("PT.Erlangga");
+        publisher.setZip("3531");
+        publisher.setState("Central Java");
+
         Book book2 = new Book();
         book2.setTitle("Cara jadi kutu buku");
         book2.setIsbn("123345");
 
+        Publisher savedPublisher = publisherRepository.save(publisher);
 
         Author messiSaved = authorRepository.save(messi);
         Book book2Saved = bookRepository.save(book2);
 
-//        authorRepository.save(ditoSaved);
-//        authorRepository.save(messiSaved);
-
         ditoSaved.getBooks().add(bookSaved);
         messiSaved.getBooks().add(book2Saved);
+
+        bookSaved.getAuthors().add(ditoSaved);
+        book2Saved.getAuthors().add(messiSaved);
+
+        book2Saved.setPublisher(savedPublisher);
+        bookSaved.setPublisher(savedPublisher);
+
         System.out.println("In Bootstrap dude ");
         System.out.println("Author Count " + authorRepository.count());
         System.out.println("Book Count " + bookRepository.count());
+
+        bookRepository.save(book2Saved);
+        bookRepository.save(bookSaved);
+
+        System.out.println("Publisher Count:" + savedPublisher);
     }
 }
