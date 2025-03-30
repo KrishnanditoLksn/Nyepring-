318ffcaa24d0077f209859b474e2ee67ba9c819c
KrishnanditoLksn <krislaksana123@gmail.com>
2025-03-30 19:45:29 +0700

    delete : service annotation

--- src/main/java/dito/nyepring/springweb/services/author/AuthorService.java
+++ src/main/java/dito/nyepring/springweb/services/author/AuthorService.java
@@ -1,11 +1,9 @@
 package dito.nyepring.springweb.services.author;
 
 import dito.nyepring.springweb.domain.Author;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Service;
 
-@Service
-interface AuthorService  {
+
+public interface AuthorService {
 
     Iterable<Author>findAllAuthor();
 }
