b5d4824a757b05b4da9ff1e3c18cff42af7716bc
KrishnanditoLksn <krislaksana123@gmail.com>
2025-03-30 19:46:10 +0700

    feat:added service annotation in implementation class

--- src/main/java/dito/nyepring/springweb/services/author/AuthorServiceImpl.java
+++ src/main/java/dito/nyepring/springweb/services/author/AuthorServiceImpl.java
@@ -2,7 +2,9 @@ package dito.nyepring.springweb.services.author;
 
 import dito.nyepring.springweb.domain.Author;
 import dito.nyepring.springweb.repositories.AuthorRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthorServiceImpl implements AuthorService {
 
     private final AuthorRepository authorRepository;
