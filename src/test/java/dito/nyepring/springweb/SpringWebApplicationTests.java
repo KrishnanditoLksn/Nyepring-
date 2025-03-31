966122bdce7e8c1cdf8c59ae07baa735a594c915
KrishnanditoLksn <krislaksana123@gmail.com>
2025-03-31 14:10:09 +0700

    test:hello

--- src/test/java/dito/nyepring/springweb/SpringWebApplicationTests.java
+++ src/test/java/dito/nyepring/springweb/SpringWebApplicationTests.java
@@ -1,13 +1,26 @@
 package dito.nyepring.springweb;
 
+import dito.nyepring.springweb.controllers.BookController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class SpringWebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Test
+    void testGetBookControllerFromCtx() {
+        BookController bookController = applicationContext.getBean(BookController.class);
+        bookController.sayHello();
+    }
+
+    @Test
+    void contextLoads() {
+
+    }
 
 }
